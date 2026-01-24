dc5c6988d4fc34a7fe23de70131dc4c77d832a12
vexinom <rafal@DESKTOP-UNLIIH9.localdomain>
2026-01-24 19:10:17 +0100

    Better parsing was added

--- commands.h
+++ commands.h
@@ -2,13 +2,17 @@
 #define COMMANDS_H
 
 #define MAX_COMMAND_ARG 64
+#define MAX_LEN_LONG_ARG 64
 
 void help();
 int cd(char* input_arg[], int* input_arg_number);
 int echo(char* input_arg[], int* input_arg_number);
 int pwd(char* input_arg[], int* input_arg_number);
 int ls(char* input_arg[], int* input_arg_number);
+int cp(char* input_arg[], int* input_arg_number);
 
 int arguments_parser(char* input_arg[], int* input_arg_number, char command_arg[], int * command_arg_number);
+
+int arguments_parser_2(char* input_arg[], int* input_arg_number, char short_arg[], int * short_arg_number, char long_arg[][MAX_LEN_LONG_ARG], int * long_arg_number);
 
 #endif

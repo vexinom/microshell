1970319f214b6f08904c6905c453af8a9ac66a9a
martyna <martyna@DESKTOP-KTQSFP3.localdomain>
2026-01-26 18:48:26 +0100

    Added ls -r

--- cp.c
+++ cp.c
@@ -6,9 +6,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h> 
+#include <dirent.h>
+#include <errno.h>
 
 int normal_copying(char* input_arg[], int* input_arg_number, int arg_size);
 int resursive_copying(char* input_arg[], int* input_arg_number, int arg_size);
+int copying(char * source, char * destination);
 
 
 int cp(char* input_arg[], int* input_arg_number)
@@ -53,7 +56,11 @@ int cp(char* input_arg[], int* input_arg_number)
         {
             if(strcmp(long_arg[i], "--help") == 0)
             {
-                printf("jjojo\n");
+                printf("\ncp - copy files and directories\n");
+                printf("cp [ARGUMENT]... [SOURCE]... [DESTINATION]\n");
+                printf("Arguments\n");
+                printf("   \033[1m-r/R  --recursive\033[0m\t - copy directories recursively\n");
+                printf("\n");
                 return 0;
             }
             else if(strcmp(long_arg[i], "--recursive") == 0)
@@ -139,7 +146,7 @@ int normal_copying(char* input_arg[], int* input_arg_number, int arg_size)
 
         while((bytes = read(source, buffor, 4096)) > 0)
         {
-            write(destination, buffor, bytes);
+            (void)!write(destination, buffor, bytes);
         }
 
         close(source);
@@ -149,11 +156,91 @@ int normal_copying(char* input_arg[], int* input_arg_number, int arg_size)
     return 0;     
 }
 
-int resursive_copying(char* input_arg[], int* input_arg_number, int arg_size)
+int copying(char * source, char * destination)
 {
-    struct stat st;
-    if(stat(input_arg[*(input_arg_number)-1], &st) < 0 || S_ISDIR(st.st_mode))
+    struct stat st2;
+    if(stat(source, &st2) < 0)
     {
         return -1;
     }
+
+    if(S_ISDIR(st2.st_mode))
+    {
+        if(mkdir(destination, 0755) < 0 && errno != EEXIST)
+        {
+            return -1;
+        }
+
+        DIR *d = opendir(source);
+
+        if(d == NULL)
+        {
+            return -1;
+        }
+        struct dirent *entry;
+
+        while((entry = readdir(d)) != NULL)
+        {
+            if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            {
+                continue;
+            }
+            char source_path[512], destination_path[512];
+            snprintf(source_path, 512, "%s/%s", source, entry->d_name);
+            snprintf(destination_path, 512, "%s/%s", destination, entry->d_name);
+
+            if(copying(source_path, destination_path) == -1)
+            {
+                closedir(d);
+                return -1;
+            }
+
+        }
+        closedir(d);
+    }
+    else
+    {
+        char cmd[1024];
+        snprintf(cmd, 1024, "cp -p '%s' '%s'", source, destination);
+        int ret = system(cmd);
+        if (ret == -1) {
+            return -1;
+        }
+        if (!WIFEXITED(ret) || WEXITSTATUS(ret) != 0) {
+            return -1;
+        }
+            }
+    return 0;
+}
+
+int resursive_copying(char* input_arg[], int* input_arg_number, int arg_size)
+{
+    struct stat st;
+    char * destination = input_arg[(*input_arg_number) - 1];
+
+    if(stat(destination, &st) < 0 || ! S_ISDIR(st.st_mode))
+    {
+        return -1;
+    }
+
+    for(int i = 1 + arg_size; i < (*input_arg_number) - 1; i++)
+    {
+        char desintation_path[512];
+        char * source_name = strrchr(input_arg[i], '/');
+        if(source_name != NULL)
+        {
+            source_name += 1;
+        }
+        else
+        {
+            source_name = input_arg[i];
+        }
+        snprintf(desintation_path, 512, "%s/%s", destination, source_name);
+        if(copying(input_arg[i], desintation_path) == -1)
+        {
+            return -1;
+        }
+    }
+    return 0;
+
 }
